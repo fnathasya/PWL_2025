294d8415f384e33a51a6b75aa91e731984129e10
fnathasya <faizaanathasya@gmail.com>
2025-03-19 11:22:56 +0700

    $fillable

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -16,11 +16,17 @@ class UserController extends Controller
 
         // tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            // 'nama' => 'Pelanggan Pertama',
             
+            'username' => 'manager_tiga',
+            'password' => Hash::make('12345'),
+            'nama' => 'Manager 3',
+            'level_id' => 2,
         ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user dengan username 'customer-1'
-
+        // UserModel::where('username', 'customer-1')->update($data); // update data user dengan username 'customer-1'
+        
+        UserModel::create($data); // Menambah data user baru
+        
         // akses model UserModel
         $user = UserModel::all(); // Mengambil semua data user
         return view('user', ['data' => $user]); // Mengirim data user ke view
