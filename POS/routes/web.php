a9d7e40a7c47bf7cbcd9daf4e3736d3f872e5421
fnathasya <faizaanathasya@gmail.com>
2025-03-19 10:43:36 +0700

    Implementasi Query Builder

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -3,6 +3,7 @@
 // Import class yang diperlukan
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\UserController;
@@ -47,3 +48,4 @@ Route::get('/', function () {
 
 // Akses URL: '/level', menjalankan method 'index' di LevelController
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('kategori', [KategoriController::class, 'index']);
