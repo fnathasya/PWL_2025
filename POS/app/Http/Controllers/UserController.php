3b78372098cc3f523335baeafe8ce70bdbe3b681
fnathasya <faizaanathasya@gmail.com>
2025-03-19 11:47:57 +0700

    Retrieving Single Models

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -15,20 +15,27 @@ class UserController extends Controller
         // return view('user.profile', compact('id', 'name'));
 
         // tambah data user dengan Eloquent Model
-        $data = [
-            // 'nama' => 'Pelanggan Pertama',
-            
-            'username' => 'manager_tiga',
-            'password' => Hash::make('12345'),
-            'nama' => 'Manager 3',
-            'level_id' => 2,
-        ];
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        //     'username' => 'manager_tiga',
+        //     'password' => Hash::make('12345'),
+        //     'nama' => 'Manager 3',
+        //     'level_id' => 2,
+        // ];
         // UserModel::where('username', 'customer-1')->update($data); // update data user dengan username 'customer-1'
         
-        UserModel::create($data); // Menambah data user baru
+        // UserModel::create($data); // Menambah data user baru
+
+        // $user = UserModel::find(1); // Mengambil data user dengan id 1
+        // $user = UserModel::where('level_id', 1)->first(); // Mengambil data user dengan level_id 1
+        // $user = UserModel::firstWhere('level_id', 1); // Mengambil data user dengan level_id 1
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        }); // Mengambil data user dengan id 1, hanya kolom username dan nama
+        return view('user', ['data' => $user]); // Mengambil nama level user
         
         // akses model UserModel
-        $user = UserModel::all(); // Mengambil semua data user
-        return view('user', ['data' => $user]); // Mengirim data user ke view
+        // $user = UserModel::all(); // Mengambil semua data user
+        // return view('user', ['data' => $user]); // Mengirim data user ke view
     }
 }
