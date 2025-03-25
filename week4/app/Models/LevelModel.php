4d8952071e1740c105d0ccd61fe9debb2dd13ce8
fnathasya <faizaanathasya@gmail.com>
2025-03-25 20:01:38 +0700

    relationship

---
+++ week4/app/Models/LevelModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class LevelModel extends Model {
+    protected $table = 'm_level';
+    public function user(): BelongsTo {
+        return $this->belongsTo(UserModel::class);
+    }
+}
